b4b5fa5bf87cce03f245cf379e0ed9bce5af6d01
yuehchou <30822495+yuehchou@users.noreply.github.com>
2020-04-30 20:15:07 +0800

    modify confusion matrix python file

--- hw3/release/confusion_matrix.py
+++ hw3/release/confusion_matrix.py
@@ -1,11 +1,12 @@
+import os, sys
 import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
-csv_fpath = None
-save_fig_path = None
+csv_fpath = './validation_prediction_and_answer.csv'
+save_fig_path = './../fig/confusion_matrix.png'
 
 def plot_confusion_matrix(
    cm,
@@ -15,35 +16,35 @@ def plot_confusion_matrix(
    title='Confusion matrix',
    cmap=plt.cm.Blues
 ):
-"""
-This function prints and plots the confusion matrix.
-Normalization can be applied by setting `normalize=True`.
-"""
-if normalize:
-   cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-   print("Normalized confusion matrix")
-else:
-   print('Confusion matrix, without normalization')
-   print(cm)
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+        print(cm)
 
-   plt.imshow(cm, interpolation='nearest', cmap=cmap)
-   plt.title(title)
-   plt.colorbar()
-   tick_marks = np.arange(len(classes))
-   plt.xticks(tick_marks, classes, rotation=45)
-   plt.yticks(tick_marks, classes)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
 
-   fmt = '.2f' if normalize else 'd'
-   thresh = cm.max() / 2.
-   for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-      plt.text(j, i, format(cm[i, j], fmt),
-               horizontalalignment="center",
-               color="white" if cm[i, j] > thresh else "black")
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
 
-   plt.ylabel('True label')
-   plt.xlabel('Predicted label')
-   plt.tight_layout()
-   plt.savefig(save_fpath)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    plt.savefig(save_fpath)
 
 
 df = pd.read_csv(csv_fpath)
